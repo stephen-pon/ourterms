700a3832be5733dd31381a8cb19cefb26c958616
Andy <andrew@andrewchan.ca>
2019-02-23 15:18:16 -0800

    sample template card added to dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,15 +4,31 @@ import { withStyles } from '@material-ui/core';
 
 
 const styles = (theme) => ({
-
+  card: {
+    width: 150,
+    display: 'inline-block',
+    margin: 20,
+    border: 'thin gold solid'
+  }
 });
 
 class Dashboard extends PureComponent {
 
   render() {
+
+    const {
+      classes
+    } = this.props;
+
     return (
       <div>
         <h1>This is the dashboard page that comes after login</h1>
+        <div className={classes.card}>
+          <img class="card-img-top" src="http://fillmurray.com/150/150" alt="Card image cap"></img>
+          <div class="card-body">Address</div>
+          <div class="card-body">Tenant Name</div>
+          <div class="card-body">Caption goes here</div>
+        </div>
       </div>
     );
   }

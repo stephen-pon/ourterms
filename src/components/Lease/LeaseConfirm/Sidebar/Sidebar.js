d9ee167b5f69b667a7bbc3f6afbf30e44b407081
Stephen Pon <sponis1@gmail.com>
2019-02-23 19:13:46 -0800

    lease confirm page sidebar

--- src/components/Lease/LeaseConfirm/Sidebar/Sidebar.js
+++ src/components/Lease/LeaseConfirm/Sidebar/Sidebar.js
@@ -1,18 +1,39 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Radio from '@material-ui/core/Radio';
 
 
 const styles = (theme) => ({
-  sidebar: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column'
+  drawerPaper: {
+    width: 300
   },
   content: {
     height: '100%',
-    backgroundColor: 'red',
-    width: '100px'
+    width: '100%',
+    padding: 10,
+    backgroundColor: 'red'
+  },
+  header: {
+    display: 'flex',
+    justifyContent: 'space-around',
+  },
+  selector: {
+    paddingLeft: 10
+  },
+  choiceGroup: {
+    display: 'flex',
+    flexDirection: 'column',
+    paddingLeft: 30
+  },
+  choice: {
+    paddingBottom: 5
+  },
+  radioButton: {
+
   }
 });
 
@@ -24,11 +45,46 @@ class Sidebar extends PureComponent {
     } = this.props;
 
     return (
-      <div className={classes.sidebar}>
-        <div className={classes.content}>
-
+      <Drawer
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        variant='permanent'
+        open
+      >
+        <div className={classes.header}>
+          <h3>Other Considerations</h3>
         </div>
-      </div>
+        <List className={classes.selector}>
+          <ListItem>Pets</ListItem>
+          <form className={classes.choiceGroup}>
+            <span className={classes.choice}>
+              <input type='radio' name='pets'></input> Yes
+            </span>
+            <span className={classes.choice}>
+              <input type='radio' name='pets'></input> No
+            </span>
+          </form>
+          <ListItem>Smoking</ListItem>
+          <form className={classes.choiceGroup}>
+            <span className={classes.choice}>
+              <input type='radio' name='smoking'></input> Yes
+            </span>
+            <span className={classes.choice}>
+              <input type='radio' name='smoking'></input> No
+            </span>
+          </form>
+          <ListItem>Waterbeds</ListItem>
+          <form className={classes.choiceGroup}>
+            <span className={classes.choice}>
+              <input type='radio' name='waterbeds'></input> Yes
+            </span>
+            <span className={classes.choice}>
+              <input type='radio' name='waterbeds'></input> No
+            </span>
+          </form>
+        </List>
+      </Drawer>
     );
   }
 }
